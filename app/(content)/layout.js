6dfe38a85ad5cbd7d095a86261b5217f974b5385
Ezz dghedy <ezzmohsend@gmail.com>
2024-12-09 14:40:05 +0200

    group route

--- app/(content)/layout.js
+++ app/(content)/layout.js
@@ -1,11 +1,10 @@
 import Header from '@/components/Header';
-import './globals.css';
+import '../globals.css';
 
 export const metadata = {
   title: 'Next.js Page Routing & Rendering',
   description: 'Learn how to route to different pages.',
 };
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
