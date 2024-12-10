1b98d915f7196e2383dfe42c02f88ee06f2d067e
Ezz dghedy <ezzmohsend@gmail.com>
2024-12-10 14:35:53 +0200

    fetching data on server side using async component

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,42 +1,62 @@
-'use client';
 import NewsList from '@/components/NewsList';
-import { DUMMY_NEWS } from '@/dummyNews';
 import React from 'react';
 
-const page = () => {
-  const [error, setError] = React.useState();
-  const [isLoading, setIsLoading] = React.useState(false);
-  const [news, setNews] = React.useState();
-  React.useEffect(() => {
-    const fetchNews = async () => {
-      setIsLoading(true);
-      const response = await fetch('http://localhost:8080/news');
-      if (!response.ok) {
-        setError('Failed to fetch news.');
-        setIsLoading(false);
-      }
-      const data = await response.json();
-      setIsLoading(false);
-      setNews(data);
-    };
-    fetchNews();
-  }, []);
-  if (isLoading) {
-    return <p>Loading...</p>;
+const page = async () => {
+  const response = await fetch('http://localhost:8080/news');
+  if (!response.ok) {
+    throw new Error('Failed to fetch news.');
   }
-  if (error) {
-    return <p>{error}</p>;
-  }
-  let newsContent;
-  if (news) {
-    newsContent = <NewsList news={news} />;
-  }
+  const news = await response.json();
+
   return (
     <>
       <h1>News Page</h1>
-      {newsContent}
+      <NewsList news={news} />;{' '}
     </>
   );
 };
 
 export default page;
+
+// client side FETCHING Data
+// 'use client';
+// import NewsList from '@/components/NewsList';
+// import React from 'react';
+
+// const page = () => {
+//   const [error, setError] = React.useState();
+//   const [isLoading, setIsLoading] = React.useState(false);
+//   const [news, setNews] = React.useState();
+//   React.useEffect(() => {
+//     const fetchNews = async () => {
+//       setIsLoading(true);
+//       const response = await fetch('http://localhost:8080/news');
+//       if (!response.ok) {
+//         setError('Failed to fetch news.');
+//         setIsLoading(false);
+//       }
+//       const data = await response.json();
+//       setIsLoading(false);
+//       setNews(data);
+//     };
+//     fetchNews();
+//   }, []);
+//   if (isLoading) {
+//     return <p>Loading...</p>;
+//   }
+//   if (error) {
+//     return <p>{error}</p>;
+//   }
+//   let newsContent;
+//   if (news) {
+//     newsContent = <NewsList news={news} />;
+//   }
+//   return (
+//     <>
+//       <h1>News Page</h1>
+//       {newsContent}
+//     </>
+//   );
+// };
+
+// export default page;
